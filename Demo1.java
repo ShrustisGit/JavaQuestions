f963ddaa1af2e8d9fd1a9f1f1d6a9396e427e24d
ShrustisGit <shrustihalli20@gmail.com>
2026-08-04 15:15:51 +0530

    Update

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,12 @@
 package JavaQuestions;
 
+import java.util.Scanner;
+
 public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
     
 }
