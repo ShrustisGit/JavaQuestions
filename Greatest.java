275fa4013a6c40e458f185dee77754859c320564
ShrustisGit <shrustihalli20@gmail.com>
2026-08-04 17:55:44 +0530

    8th Question

--- Greatest.java
+++ Greatest.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class Greatest {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int num1=sc.nextInt();
-        int num2=sc.nextInt();
-        if(num1 > num2){
-            System.out.println("Number 1 is greater");
+        // int num1=sc.nextInt();
+        // int num2=sc.nextInt();
+        // if(num1 > num2){
+        //     System.out.println("Number 1 is greater");
+        // }
+        // else{
+        //     System.out.println("Number 2 is greater");
+        // }
+
+        // Lagest of 3
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a>b && a>c){
+            System.out.println("1st number is greater");
+        }
+        else if(b>a && b>c){
+            System.out.println("2nd number is greater");
         }
         else{
-            System.out.println("Number 2 is greater");
+            System.out.println("3rd number is greater");
         }
     }
 }
