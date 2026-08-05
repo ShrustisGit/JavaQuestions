5de34cd38711e9cadece6e54d599fc12f979f3d6
ShrustisGit <shrustihalli20@gmail.com>
2026-08-05 15:07:25 +0530

    11th Question

---
+++ PrintuntilNth.java
@@ -0,0 +1,13 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class PrintuntilNth {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i =1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
