7c29817ca9d97766c074877a67830261a9f1e341
ShrustisGit <shrustihalli20@gmail.com>
2026-08-06 17:46:06 +0530

    17th Question

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int reverse =0;
+        int original=n;
+        while(n>0){
+            int digit=n%10;
+            n=n/10;
+            reverse = reverse * 10 + digit;
+        }
+        if(original == reverse){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
