fe2556cf811241950f99d4feae2aebc7d9bce01f
ShrustisGit <shrustihalli20@gmail.com>
2026-08-07 22:14:33 +0530

    5th Question

--- YearLeap.java
+++ YearLeap.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class YearLeap {
     public static void main(String[] args) {
+    System.out.println("Year LEap");
     Scanner sc=new Scanner(System.in);
     int year=sc.nextInt();
     if(year % 4 == 0){
