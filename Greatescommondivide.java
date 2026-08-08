95063430663ea20e0c4aa8d65921879b8e290d17
ShrustisGit <shrustihalli20@gmail.com>
2026-08-08 12:07:08 +0530

    23 Question

---
+++ Greatescommondivide.java
@@ -0,0 +1,26 @@
+package JavaQuestions;
+import java.util.Scanner;
+
+public class Greatescommondivide {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int gcd = 1;
+        int greater = Math.max(a, b);
+
+        for (int i = 1; i <= greater; i++) {
+
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD = " + gcd);
+
+        sc.close();
+    }
+}
