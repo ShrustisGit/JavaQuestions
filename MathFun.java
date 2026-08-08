e64acaba78c6f49701be65314aa6df806dbcce3c
ShrustisGit <shrustihalli20@gmail.com>
2026-08-08 23:08:45 +0530

    22th Question

--- MathFun.java
+++ MathFun.java
@@ -2,6 +2,7 @@ package JavaQuestions;
 
 public class MathFun {
     public static void main(String[] args) {
+        System.out.println("Math functions");
         System.out.println(Math.pow(2, 5));
         System.out.println(Math.sqrt(5));
         System.out.println(Math.cbrt(27));
