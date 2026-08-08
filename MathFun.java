c47a881460ee3d4348242b36b07328e478580e52
ShrustisGit <shrustihalli20@gmail.com>
2026-08-08 11:56:55 +0530

    22th Question

---
+++ MathFun.java
@@ -0,0 +1,16 @@
+package JavaQuestions;
+
+public class MathFun {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2, 5));
+        System.out.println(Math.sqrt(5));
+        System.out.println(Math.cbrt(27));
+        System.out.println(Math.abs(-20));
+        System.out.println(Math.max(10, 50));
+        System.out.println(Math.min(20, -5));
+        System.out.println(Math.round(.09));
+        System.out.println(Math.ceil(4.5));
+        System.out.println(Math.floor(8.7));
+        System.out.println(Math.random());
+    }
+}
