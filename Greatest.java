ca563ec6955e67b52b28229258590a45aa09a202
ShrustisGit <shrustihalli20@gmail.com>
2026-08-10 21:50:38 +0530

    8th Question

--- Greatest.java
+++ Greatest.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Greatest {
     public static void main(String[] args) {
+        System.out.println("Greater");
         Scanner sc=new Scanner(System.in);
         // int num1=sc.nextInt();
         // int num2=sc.nextInt();
