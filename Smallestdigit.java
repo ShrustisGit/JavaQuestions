f264fbecdbdfe540fd65ece405f7d0833ee539ac
ShrustisGit <shrustihalli20@gmail.com>
2026-08-10 21:52:48 +0530

    28th Question

--- Smallestdigit.java
+++ Smallestdigit.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Smallestdigit {
     public static void main(String[] args) {
+        System.out.println("Smallest digit");
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int max=9;
