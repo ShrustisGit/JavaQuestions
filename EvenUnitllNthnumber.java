a224261a888278fd374a05b88c4d7096d61585d1
ShrustisGit <shrustihalli20@gmail.com>
2026-08-10 21:49:39 +0530

    26th Question

--- EvenUnitllNthnumber.java
+++ EvenUnitllNthnumber.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class EvenUnitllNthnumber {
     public static void main(String[] args) {
+        System.out.println("Even until Nth");
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i =1 ;i < n ; i++){
