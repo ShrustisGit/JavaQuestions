44bdd4f8366bab39676d9361ec2b7124557f2695
ShrustisGit <shrustihalli20@gmail.com>
2026-08-11 21:44:40 +0530

    31th Question

---
+++ Countzeros.java
@@ -0,0 +1,19 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class Countzeros {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        while (n>0) {
+            int digit=n%10;
+            n=n/10;
+            if(digit == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
