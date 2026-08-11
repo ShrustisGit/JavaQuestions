48fbba1d030b13fab9f0ca8c2ccfcbed1165ce99
ShrustisGit <shrustihalli20@gmail.com>
2026-08-11 21:48:51 +0530

    32th Question

---
+++ Sumofevendigit.java
@@ -0,0 +1,20 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class Sumofevendigit {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        while (n>0) {
+            int digit=n%10;
+            n=n/10;
+            if(digit % 2 == 0){
+                sum += digit;
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
