4e23459a3fd5ac256abe960b18a5d506ea429200
ShrustisGit <shrustihalli20@gmail.com>
2026-08-11 21:57:34 +0530

    35th Question

---
+++ PrimeNumbers.java
@@ -0,0 +1,30 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int num = 2; num <= n; num++) {
+
+            int count = 0;
+
+            for (int i = 1; i <= num; i++) {
+
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(num + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
