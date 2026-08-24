3609264d36e21b111efee2aa27e3444fea1b03cf
ShrustisGit <shrustihalli20@gmail.com>
2026-08-24 06:14:11 +0530

    62th Occerence of Array

--- OccerenceArr.java
+++ OccerenceArr.java
@@ -1,5 +1,16 @@
 package JavaQuestions;
 
 public class OccerenceArr {
-    
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,4,4,4,6,4};
+        int target=4;
+        int count=0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]==target){
+                count++;
+            }
+
+        }
+        System.out.println("Occurnce of"+" "+ target+ " "+ count);
+    }
 }
