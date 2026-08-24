967abcb033e3c2d1eb7b0d301b394038f82e470d
ShrustisGit <shrustihalli20@gmail.com>
2026-08-24 06:18:23 +0530

    63th Largest from Array

---
+++ LargetinArr.java
@@ -0,0 +1,14 @@
+package JavaQuestions;
+
+public class LargetinArr {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9,11,55,999,777,665,50};
+        int max=0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
