cdb06e0e788674a1bc8972f10a8ec89918f01922
ShrustisGit <shrustihalli20@gmail.com>
2026-08-25 06:32:42 +0530

    67th Remove Duplicate from Sorted

---
+++ RemoveDuplicate.java
@@ -0,0 +1,30 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Size Of Array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i =0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i =0;i<n;i++){
+            boolean duplicate = false;
+            for(int j=0;j<i;j++){
+                
+                if(arr[i]==arr[j]){
+                    duplicate=true;
+                    break;
+                }
+            }
+            if(!duplicate){
+                System.out.print(arr[i]);
+            }
+
+        }
+
+}
+ }
