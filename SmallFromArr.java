c58afa6f141c62c3b10dd6bdecc80f7405b90494
ShrustisGit <shrustihalli20@gmail.com>
2026-08-25 05:50:16 +0530

    65th Min from Sorted

---
+++ SmallFromArr.java
@@ -0,0 +1,22 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class SmallFromArr {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Size Of Array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+         for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int min=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
