8694885057a86fede9e17f3aca03531759baaf51
Shrusti Halli <shrustihalli20@gmail.com>
2026-08-28 16:37:54 +0530

    Update EvenandOddFromArr.java

--- EvenandOddFromArr.java
+++ EvenandOddFromArr.java
@@ -2,6 +2,7 @@ package JavaQuestions;
 
 public class EvenandOddFromArr {
     public static void main(String[] args) {
+        System.out.println("Even Odd In Array ";
         int[] arr={1,2,3,4,5,6,7,8,9,10};
         int even=0;
         int odd=0;
