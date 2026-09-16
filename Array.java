1ab1aecebdea5a2305b87216f2e18e14c9582290
ShrustisGit <shrustihalli20@gmail.com>
2026-09-16 06:51:43 +0530

    Array

--- Array.java
+++ Array.java
@@ -1,7 +1,21 @@
 package JavaQuestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
-        int [] roll=new int[5];
-        System.out.println(roll[0]);
+        Scanner sc=new Scanner(System.in);
+        int [] arr=new int[5];
+        // System.out.println(arr[0]);
+
+        for(int i =0 ; i< arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        // for(int num :arr){
+        //     System.out.println(num);
+        // }
+
     }
 }
