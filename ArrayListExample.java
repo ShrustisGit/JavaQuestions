f12590c83365dbf6353b4baedd44b6047dbbdee6
ShrustisGit <shrustihalli20@gmail.com>
2026-09-16 09:29:24 +0530

    ArrayList

---
+++ ArrayListExample.java
@@ -0,0 +1,14 @@
+package JavaQuestions;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+      ArrayList<Integer> list=new ArrayList<>();
+      list.add(10);
+      list.add(20);
+      list.add(30);
+      System.out.println(list);
+    }
+}
