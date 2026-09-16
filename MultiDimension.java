93e53aaa8a846433b96a505d141363e881bbc3cb
ShrustisGit <shrustihalli20@gmail.com>
2026-09-16 09:18:10 +0530

    2D Arry

---
+++ MultiDimension.java
@@ -0,0 +1,24 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);   
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+    */  
+   int[][] arr=new int[3][3];
+   for(int row=0; row<arr.length;row++){
+        
+        for(int col=0;col<arr[row].length;col++){
+            arr[row][col]=sc.nextInt();
+            System.out.print(arr[row][col]+ " ");
+        }
+        System.out.println();
+   }
+   
+}
+ }
