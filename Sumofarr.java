cbea41ccdbe2d8d453c965d9a2f5651437f43b06
ShrustisGit <shrustihalli20@gmail.com>
2026-09-17 20:38:45 +0530

    Sum Of Array

--- Sumofarr.java
+++ Sumofarr.java
@@ -2,12 +2,12 @@ package JavaQuestions;
 
 public class Sumofarr {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
+        int[] array={1,2,3,4,5};
         int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum=sum+arr[i];
+        for(int i =0;i<array.length;i++){
+            sum=sum+array[i];
         }
-        System.out.println(sum);
+        System.out.println("Sum of Array" + " " + sum);
     }
     
 }
