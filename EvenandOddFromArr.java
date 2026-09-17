c588017ed9977c4a5b1c76b2dc46ba2f0788aaaf
ShrustisGit <shrustihalli20@gmail.com>
2026-09-17 20:58:03 +0530

    Even Odd Array

--- EvenandOddFromArr.java
+++ EvenandOddFromArr.java
@@ -3,18 +3,19 @@ package JavaQuestions;
 public class EvenandOddFromArr {
     public static void main(String[] args) {
         System.out.println("Even Odd In Array ");
-        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        int[] array={2,3,4,5,6,7,8,9,10};
         int even=0;
         int odd=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]%2 == 0){
+        for(int i=0;i<array.length;i++){
+            if(array[i] % 2 == 0){
                 even++;
             }
             else{
                 odd++;
             }
+
         }
-        System.out.println("Even"+" " +even);
+        System.out.println("Even"+" "+even);
         System.out.println("Odd"+" "+odd);
     }
 }
