915e4359ab13379e03f969832966c09ffebe314b
ShrustisGit <shrustihalli20@gmail.com>
2026-09-19 08:09:52 +0530

    Prime factors

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -6,25 +6,17 @@ public class PrimeNumbers {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        for (int num = 2; num <= n; num++) {
-
-            int count = 0;
-
-            for (int i = 1; i <= num; i++) {
-
-                if (num % i == 0) {
+        int n=sc.nextInt();
+        for(int i =2;i<=n;i++){
+            int count=0;
+            for(int num=1;num<=i;num++){
+                if(i%num == 0){
                     count++;
                 }
             }
-
-            if (count == 2) {
-                System.out.print(num + " ");
+            if(count == 2){
+                System.out.print(i+" ");
             }
         }
-
-        sc.close();
     }
 }
